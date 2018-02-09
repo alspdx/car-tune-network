f22b41ebe1d56f9d1ea893664606796104691320
Adam <alspdx@gmail.com>
2018-02-09 13:01:41 -0800

    add form to newcar component, some styling for form layout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,6 +10,7 @@ function App(){
           --fog: #E7E6E1;
           --cream: #F7F6E7;
           --bayblue: #537791;
+          --deepash: #3f3f3c;
         }
         html {
           box-sizing: border-box;
@@ -19,6 +20,7 @@ function App(){
         }
         body {
           background: var(--fog);
+          color: var(--deepash);
         }
       `}</style>
       <Header />
--- src/components/NewCar.jsx
+++ src/components/NewCar.jsx
@@ -3,6 +3,50 @@ import React from 'react';
 function NewCar() {
   return(
     <div>
+      <style jsx>{`
+        form {
+          display: flex;
+          flex-direction: column;
+        }
+        label {
+          display: flex;
+          flex-direction: column;
+          padding-bottom: 20px;
+        }
+
+      `}</style>
+      <h3>New car entry form:</h3>
+      <form>
+        <label>
+          Year:
+          <input type='text'/>
+        </label>
+        <label>
+          Make:
+          <input type='text'/>
+        </label>
+        <label>
+          Model:
+          <input type='text'/>
+        </label>
+        <label>
+          Color:
+          <input type='text'/>
+        </label>
+        <label>
+          Customer:
+          <input type='text'/>
+        </label>
+        <label>
+          Reason(s) for service:
+          <textarea type='text'/>
+        </label>
+        <label>
+          Make:
+          <input type='text'/>
+        </label>
+        <input type='submit' value='Submit'/>
+      </form>
     </div>
   );
 }
